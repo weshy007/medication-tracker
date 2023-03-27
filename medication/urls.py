1e1810cff1ae1dad3281c97b0721740fed45783c
Weshy Joe <josephwaweru96@gmail.com>
2023-03-27 20:03:52 +0300

    medication views and urls

--- medication/urls.py
+++ medication/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('medications/', views.MedicationListView.as_view(), name='medication-list'),
+    path('medications/create/', views.MedicationCreateView.as_view(), name='medication-create'),
+    path('medications/<int:pk>/update/', views.MedicationUpdateView.as_view(), name='medication-update'),
+    path('medications/<int:pk>/delete/', views.MedicationDeleteView.as_view(), name='medication-delete'),
+    path('medication_logs/', views.MedicationLogView.as_view(), name='medication-log'),
+    path('medication_logs/create/', views.MedicationLogCreateView.as_view(), name='medication-log-create'),
+    path('reminders/create/', views.ReminderCreateView.as_view(), name='reminder-create'),
 ]
--- medication/views.py
+++ medication/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Medication, MedicationLog, Reminder
+from .serializers import MedicationSerializer, MedicationLogSerializer, ReminderSerializer
+
 
 # Create your views here.
+class MedicationListView(generics.ListAPIView):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+    permission_classes = [IsAuthenticated, ]
+
+
+class MedicationCreateView(generics.CreateAPIView):
+    serializer_class = MedicationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MedicationUpdateView(generics.UpdateAPIView):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+    permission_classes = [IsAuthenticated, ]
+
+
+class MedicationDeleteView(generics.DestroyAPIView):
+    queryset = Medication.objects.all()
+    permission_classes = [IsAuthenticated, ]
+
+
+class MedicationLogView(generics.ListAPIView):
+    queryset = MedicationLog.objects.all()
+    serializer_class = MedicationLogSerializer
+    permission_classes = [IsAuthenticated, ]
+
+
+class MedicationLogCreateView(generics.CreateAPIView):
+    serializer_class = MedicationLogSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ReminderCreateView(generics.CreateAPIView):
+    serializer_class = ReminderSerializer
+    permission_classes = [IsAuthenticated]
