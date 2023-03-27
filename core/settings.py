fab4942a11d82ea62349f749db409ee2b746a877
Weshy Joe <josephwaweru96@gmail.com>
2023-03-27 20:38:30 +0300

    email notification

--- core/settings.py
+++ core/settings.py
@@ -156,3 +156,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- medication/views.py
+++ medication/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from django.conf import settings
+from django.core.mail import send_mail
+from django.utils import timezone
+from datetime import datetime, timedelta
+from rest_framework.response import Response
 
+from .tasks import send_reminder_email
 from .models import Medication, MedicationLog, Reminder
 from .serializers import MedicationSerializer, MedicationLogSerializer, ReminderSerializer
 
@@ -40,6 +47,47 @@ class MedicationLogCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
 
-class ReminderCreateView(generics.CreateAPIView):
+class ReminderCreateView(APIView):
     serializer_class = ReminderSerializer
-    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            medication = serializer.validated_data["medication"]
+            reminder_time = serializer.validated_data["reminder_time"]
+            enabled = serializer.validated_data['enabled']
+
+            # Save the reminder to db
+            reminder = Reminder.objects.create(
+                medication=medication,
+                reminder_time=reminder_time,
+                enabled=enabled
+            )
+
+            # Send reminder email
+            subject = 'Medication Reminder'
+            message = f"Remember to take {medication.name} at {reminder_time} today!"
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [request.user.email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+
+            # Schedule reminder push notification using Celery
+            now = timezone.now()
+            notification_time = datetime.combine(now.date(), reminder_time)
+
+            if notification_time < now:
+                notification_time = notification_time + timedelta(days=1)
+            send_reminder_email.apply_async(args=[medication.name, notification_time.isoformat()])
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def send_notification(medication_name, notification_time):
+    # TODO: push notification
+    # To send push notifications, you can use a push notification service like
+    # Firebase Cloud Messaging(FCM).
+    pass
+
+
